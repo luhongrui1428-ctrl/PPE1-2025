4b1831fffa74660a51bb995550bf55677db81375
luhongrui1428 <luhongrui1428@gmail.com>
2025-10-20 21:17:53 +0200

    Exercice 4: boucle for

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
 
+datadir="ann"
 if [ $# -ne 0 ]; then
     echo "Erreur : Ce script ne nécessite aucun argument"
     exit
 fi
 
-echo "Nombre de Locations en 2016:"
-count2016=$(cat "ann/2016/"* | grep "Location" | wc -l)
-echo "$count2016"
-
-echo "Nombre de Locations en 2017:"
-count2017=$(cat "ann/2017/"* | grep "Location" | wc -l)
-echo "$count2017"
-
-echo "Nombre de Locations en 2018:"
-count2018=$(cat "ann/2018/"* | grep "Location" | wc -l)
-echo "$count2018"
+for annee in 2016 2017 2018
+do
+    if [ -d "$datadir/$annee" ]; then
+        count=$(cat "$datadir/$annee/"* | grep "Location" | wc -l)
+        echo "Nombre de Locations en $annee: $count"
+    else
+        echo "$datadir/$annee n'est pas le bon dossier"
+        exit
+    fi
+done
